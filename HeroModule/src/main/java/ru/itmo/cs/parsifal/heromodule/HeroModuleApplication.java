ab07614c6511110d9ce7a1f6e00bc5e06ccb15c6
danil <danilaor2009@gmail.com>
2025-10-31 17:58:44 +0300

    added work on wildfly

--- HeroModule/src/main/java/ru/itmo/cs/parsifal/heromodule/HeroModuleApplication.java
+++ HeroModule/src/main/java/ru/itmo/cs/parsifal/heromodule/HeroModuleApplication.java
@@ -1,18 +1,19 @@
 package ru.itmo.cs.parsifal.heromodule;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 @SpringBootApplication
-public class HeroModuleApplication {
+public class HeroModuleApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HeroModuleApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HeroModuleApplication.class, args);
@@ -21,31 +22,5 @@ public class HeroModuleApplication {
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
-    }
-
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-        return tomcat;
-    }
-
-    private Connector redirectConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        connector.setRedirectPort(15479);
-        return connector;
     }
 }
--- HeroModule/src/main/java/ru/itmo/cs/parsifal/heromodule/config/SecureRestTemplateConfig.java
+++ HeroModule/src/main/java/ru/itmo/cs/parsifal/heromodule/config/SecureRestTemplateConfig.java
@@ -5,39 +5,28 @@ import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.client5.http.io.HttpClientConnectionManager;
 import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.TrustAllStrategy;
 import org.apache.hc.core5.ssl.SSLContextBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
-import java.security.cert.X509Certificate;
+import java.time.Duration;
 
 @Configuration
 public class SecureRestTemplateConfig {
 
-    @Value("${server.ssl.key-alias:tomcat}")
-    private String keyAlias;
-
     @Bean
     public RestTemplate restTemplate() throws Exception {
         SSLContext sslContext = SSLContextBuilder.create()
-                .loadTrustMaterial((chain, authType) -> {
-                    if (chain != null && chain.length > 0) {
-                        X509Certificate cert = chain[0];
-                        System.out.println("Certificate Subject: " + cert.getSubjectX500Principal());
-                        System.out.println("Certificate Alias: " + keyAlias);
-
-                        return true;
-                    }
-                    return false;
-                })
+                .loadTrustMaterial(TrustAllStrategy.INSTANCE)
                 .build();
 
         HttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
-                .setTlsSocketStrategy(new DefaultClientTlsStrategy(sslContext))
+                .setTlsSocketStrategy(new DefaultClientTlsStrategy(sslContext, NoopHostnameVerifier.INSTANCE))
                 .build();
 
         CloseableHttpClient httpClient = HttpClients.custom()
@@ -46,8 +35,8 @@ public class SecureRestTemplateConfig {
                 .build();
 
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        factory.setConnectTimeout(java.time.Duration.ofMillis(5000));
-        factory.setReadTimeout(java.time.Duration.ofMillis(15000));
+        factory.setConnectTimeout(Duration.ofMillis(5000));
+        factory.setReadTimeout(Duration.ofMillis(15000));
 
         return new RestTemplate(factory);
     }
