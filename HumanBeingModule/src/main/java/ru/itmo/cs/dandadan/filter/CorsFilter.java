be80fd5a673e4abfd4c2544512fa117447889166
Ariguun Bolorbold <bolorboldariguun16@gmail.com>
2025-11-17 18:14:02 +0300

    work: :construction: attempt to restrict HTTP2

--- HumanBeingModule/src/main/java/ru/itmo/cs/dandadan/filter/CorsFilter.java
+++ HumanBeingModule/src/main/java/ru/itmo/cs/dandadan/filter/CorsFilter.java
@@ -3,6 +3,7 @@ package ru.itmo.cs.dandadan.filter;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
@@ -10,8 +11,12 @@ public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) {
+        if ("OPTIONS".equalsIgnoreCase(requestContext.getMethod())) {
+            responseContext.setStatus(Response.Status.OK.getStatusCode());
+            return;
+        }
+        responseContext.getHeaders().putSingle("X-Protocol", "HTTP/1.1");
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Max-Age", "604800");
         responseContext.getHeaders().add("Access-Control-Allow-Headers",
                 "origin, content-type, accept, authorization");
