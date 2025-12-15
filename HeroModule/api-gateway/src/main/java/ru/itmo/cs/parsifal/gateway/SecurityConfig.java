90d1b3cc6e1424978c6b1a9b86fbc55011280bb3
danil <danilaor2009@gmail.com>
2025-12-15 13:45:31 +0300

    fixed issues with certificates

--- HeroModule/api-gateway/src/main/java/ru/itmo/cs/parsifal/gateway/SecurityConfig.java
+++ HeroModule/api-gateway/src/main/java/ru/itmo/cs/parsifal/gateway/SecurityConfig.java
@@ -1,4 +1,4 @@
-package ru.itmo.cs.parsifal.heroservice.config;
+package ru.itmo.cs.parsifal.gateway;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,17 +8,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .requiresChannel().anyRequest().requiresSecure()
-                .and()
-                .csrf().disable()
-                .authorizeRequests().anyRequest().permitAll()
-                .and()
-                .headers().httpStrictTransportSecurity()
-                .includeSubDomains(true).preload(true)
-                .maxAgeInSeconds(31536000);
+        http.requiresChannel()
+                .anyRequest().requiresSecure();
     }
 }
